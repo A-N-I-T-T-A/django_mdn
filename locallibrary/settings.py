69e39ee9a4258f453077ad3daed8b2162abf6efc
A-N-I-T-T-A <133679742+A-N-I-T-T-A@users.noreply.github.com>
2025-03-13 13:11:25 +0530

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nd^4bop*_6spyr%_7&(-wt^oa@s8%ynjib@v#y%y#swis0e3j%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-mdn.onrender.com']
 
 
 # Application definition
